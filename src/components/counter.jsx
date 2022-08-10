38d690ad5a3a28b29865c835aafb27dcd3fb4a8a
Tawfiq-Abu <tawfiqabubakr7@gmail.com>
2022-08-10 12:39:44 +0000

    learnt about how to bind events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,6 +10,12 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+//   constructor (){
+//     super();
+//     //bind returns a new instance of the function so we have to set it to that instance to use it
+//     this.handleIncrement=this.handleIncrement.bind(this);
+//   }
+
 
   renderLanguages (){
     if (this.state.languages.length === 0){
@@ -25,18 +31,29 @@ class Counter extends Component {
 
   }
 
+  handleIncrement= ()=>{
+    console.log('Increment Clicked',this);
+    this.state.count +=1
+    console.log(this.state.count)
+  }
+
   render() {
     return (
       <React.Fragment>
-        {this.state.languages.length === 0 && "please create tags"}
-        { this.renderLanguages() }
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
    
+  getBadgeClasses(){
+    let classes = 'badge m2 badge-';
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
   formatCount () {
-    const {count} = this.state;
+    const count = this.state.count;
     if (count === 0){
         return <h1 style={this.styles}>Zero</h1>
     } else{
